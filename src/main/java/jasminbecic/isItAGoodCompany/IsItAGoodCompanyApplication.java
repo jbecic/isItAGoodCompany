4a160e3e7fd7e50e536dca8ab7d12f6b331357a6
jasminbecic <becicjasmin1@gmail.com>
2022-04-12 08:37:54 -0500

    initial commit

---
+++ src/main/java/jasminbecic/isItAGoodCompany/IsItAGoodCompanyApplication.java
@@ -0,0 +1,13 @@
+package jasminbecic.isItAGoodCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsItAGoodCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsItAGoodCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jasminbecic/isItAGoodCompany/IsItAGoodCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package jasminbecic.isItAGoodCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsItAGoodCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
